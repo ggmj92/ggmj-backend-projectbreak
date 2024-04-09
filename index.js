87603402c129b14756f9ae7db3a5c3f7c32b0766
Gabriel Garcia-Miro Jeri <ggmj92@gmail.com>
2024-04-09 16:50:37 -0500

    Added new database configuration

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express'); 
+const express = require('express');
+const dbConnection = require('./config/config'); 
 const app = express();
 require('dotenv').config();  // To use environment variables
 PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => (res.send("GGMJ's Backend Project")))
+
+dbConnection();
 
 app.listen(PORT, () => {
     console.log(`Express listening on port http://localhost:${PORT}`);
